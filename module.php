0bc1472e97c2b952b376ffb4e1f8ea0419a96666
nadine-shi <nadine@afterlogic.com>
2016-08-06 20:19:03 +0300

    DbAuth - create account/change password tab when user is authenticated

--- module.php
+++ module.php
@@ -2,18 +2,17 @@
 
 class MobileSyncModule extends AApiModule
 {
+	/**
+	 * Collects the information about mobile sync from other modules and returns it.
+	 * 
+	 * @return array
+	 */
 	public function GetInfo()
 	{
 		$mResult = array();
 		
 		$this->broadcastEvent('GetInfo', array(&$mResult));
 
-		$aLoginList = array();
-		
-		$this->broadcastEvent('GetUserAuthAccountLogin', array(&$aLoginList));
-		
-		$mResult['LoginList'] = $aLoginList;
-		
 		return $mResult;
 	}
 	
