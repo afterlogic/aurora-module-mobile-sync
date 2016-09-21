911c23b8c0adcd0ac74a1503814c1113d1243dbd
nadine-shi <nadine@afterlogic.com>
2016-09-21 13:53:10 +0300

    MobileSync - comments for automatic generation of documentation

--- module.php
+++ module.php
@@ -1,7 +1,24 @@
 <?php
+/*
+ * @copyright Copyright (c) 2016, Afterlogic Corp.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
 class MobileSyncModule extends AApiModule
 {
+	/***** public functions might be called with web API *****/
 	/**
 	 * Collects the information about mobile sync from other modules and returns it.
 	 * 
@@ -14,8 +31,8 @@ class MobileSyncModule extends AApiModule
 		$mResult = array();
 		
 		$this->broadcastEvent('GetInfo', array(&$mResult));
-
+		
 		return $mResult;
 	}
-	
+	/***** public functions might be called with web API *****/
 }
