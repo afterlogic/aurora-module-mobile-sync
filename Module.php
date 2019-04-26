468e9d8e19293fdc0d8e092a6641ac31ab761aa5
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:02:31 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\MobileSync;
 
 /**
+ * Allows for syncing data with mobile devices or any application/platform which offers CalDAV/CardDAV support.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
